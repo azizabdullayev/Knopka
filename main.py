e0393c9397ead209181597c55ba19e23d63a484e
User <azizbekabdullayev2828@gmail.com>
2023-03-28 18:07:47 +0500

    first commit

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
 from aiogram import executor
 from config import dp
 from aiogram.types import Message
-from repleykeyboards import start_btn
+from repleykeyboards import start_btn, taq
+
 
 @dp.message_handler(commands=['start'])
 async def start(msg: Message):
     image = open('fotos/rasm.jpg', 'rb')
     text = f"Xush kelibsiz! {msg.from_user.full_name}"
-    await msg.answer_photo(image, caption=text , reply_markup=start_btn)
+    await msg.answer_photo(image, caption=text, reply_markup=start_btn)
+
 
 @dp.message_handler(text='BOSHQA NARSALAR')
 async def found(msg: Message):
-    img = open('fotos/foto иди домой.jpg', 'rb')
+    img = open('fotos/not foto.webp', 'rb')
     await msg.answer_photo(img)
 
 
 @dp.message_handler(text='KITOBLAR')
 async def kitob(msg: Message):
+    ttg = f"Kitob tanlang!\n1. НЕ ТУПИ\n2. БУТЬ ЛУЧШЕЙ ВЕРСИЕЙ СЕБЯ\n3. СМЫСЛ СУЩЕСТВОВАНИЯ ЧЕЛОВЕКА\n"
+    await msg.answer(ttg, reply_markup=taq)
+
+
+@dp.message_handler(text='1')
+async def bir(msg: Message):
+    await msg.answer_document(open('book/kitob1.pdf', 'rb'))
+
+
+@dp.message_handler(text='2')
+async def ikki(msg: Message):
     await msg.answer_document(open('book/kitob.pdf', 'rb'))
+
+
+@dp.message_handler(text='3')
+async def uch(msg: Message):
+    await msg.answer_document(open('book/kitob2.pdf', 'rb'))
+
+
+@dp.message_handler(text='Ortga')
+async def ortga(msg: Message):
+    ttg2 = f"Kitob tanlang!\n1. НЕ ТУПИ\n2. БУТЬ ЛУЧШЕЙ ВЕРСИЕЙ СЕБЯ\n3. СМЫСЛ СУЩЕСТВОВАНИЯ ЧЕЛОВЕКА\n"
+    await msg.answer(ttg2, reply_markup=taq)
 
 
 if __name__ == '__main__':
--- repleykeyboards.py
+++ repleykeyboards.py
@@ -10,3 +10,16 @@ start_btn = ReplyKeyboardMarkup(
 
     resize_keyboard=True
 )
+taq = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="1"),
+            KeyboardButton(text="2"),
+            KeyboardButton(text="3")
+        ],
+        [
+            KeyboardButton(text='Ortga')
+        ]
+    ],
+    resize_keyboard=True
+)
